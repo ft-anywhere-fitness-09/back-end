6e84aeb3d00221a881d73119be2ef1563cd1a8d7
sooof <sooof@me.com>
2022-02-02 11:50:01 -0800

    api/auth/register work

--- api/data/seeds/02-roles.js
+++ api/data/seeds/02-roles.js
@@ -1,15 +1,7 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('user_role').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('user_role').insert([
-        { role_type: 'instructor' },
-        { role_type: 'client' }
-      ]);
-    });
+
+exports.seed = function(knex, Promise) {
+  return knex('user_role').insert([   
+    { role_type: 'instructor' },
+    { role_type: 'client' }
+  ]);
 };
