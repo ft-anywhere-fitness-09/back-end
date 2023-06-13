29f7c3b774cc4c68e106c8cf5fcbb94e0926b437
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2023-06-13 13:23:42 -0400

    first commit

--- api/server.js
+++ api/server.js
@@ -1,37 +1,36 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-require('dotenv').config()
-const path = require('path')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
 
-const authRouter = require('./auth/auth-router.js')
+const authRouter = require("./auth/auth-router.js");
 const usersRouter = require("./users/users-router.js");
-const classesRouter = require("./classes/classes-router")
+const classesRouter = require("./classes/classes-router");
 
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
-
-server.use('/api/auth', authRouter)
+server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
-server.use('/api/classes', classesRouter)
+server.use("/api/classes", classesRouter);
 
-server.use(express.static(path.join(__dirname, 'client/build')))
-server.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'client', 'index.html'))
-})
+server.use(express.static(path.join(__dirname, "client/build")));
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
 
-server.get('/hello', (req, res) => {
-  res.status(200).json('hello world!!!!!!')
-})
+server.get("/hello", (req, res) => {
+  res.status(200).json("hello world!!!!!!");
+});
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
-  })
-})
-module.exports = server
+  });
+});
+module.exports = server;
